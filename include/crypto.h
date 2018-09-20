9e6decddf8453683c91ce12600eb90b8e36103c1
wlonga <32339853+wlonga@users.noreply.github.com>
2018-09-20 13:43:16 -0700

    Transitioning to repository provider vs storage provider

--- include/crypto.h
+++ include/crypto.h
@@ -1,31 +1,96 @@
+/**
+ ****************************************************************************************
+ *
+ * @file crypto.h
+ *
+ * @XY4 project source code.
+ *
+ * @brief primary crypto routines for the XY4 firmware.
+ *
+ * Copyright (C) 2017 XY - The Findables Company
+ *
+ * This computer program includes Confidential, Proprietary Information of XY. 
+ * All Rights Reserved.
+ *
+ ****************************************************************************************
+ */
+
 #ifndef XYCrypto
+
+/*
+ * DEFINES
+ ****************************************************************************************
+ */
+
+#define XYCrypto
+
+/*
+ * INCLUDES
+ ****************************************************************************************
+ */
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 #include "xyobject.h"
+
+/*
+ * TYPE DEFINITIONS
+ ****************************************************************************************
+ */
+
 typedef struct Signer Signer;
 typedef struct CryptoCreator CryptoCreator;
+
 typedef struct{
+  
   char* publicKey;
   char* privateKey;
 } keyPairStruct;
-#define XYCrypto
-#endif
+
+
+/*
+ * STRUCTURES
+ ****************************************************************************************
+ */
 
 struct Signer{
+  
   struct ByteArray* publicKey; // Cryptographic Public Key
   struct ByteArray* privateKey; // Cryptographic Private Key
   XYResult* (*getPublicKey)(Signer*);  // Returns public key
   XYResult* (*sign)(Signer*, struct ByteArray*); // Returns signed byte array
+  
   /*
    * The method will take data and a cryptographic signature and a cryptographic public key
    * and determine if data was signed by the given public key correctly or if the signature
    * is malformed / invalid. Boolean return value.
    */
-  int               (*verify)(Signer*, struct ByteArray* data, struct ByteArray* sig, struct ByteArray* pubkey);
+  
+  int (*verify)(Signer*, struct ByteArray* data, struct ByteArray* sig, struct ByteArray* pubkey);
   ByteArray* (*encrypt)(struct Signer*, struct ByteArray*); // Encrypt the data to the key of this Signer object
   ByteArray* (*decrypt)(struct Signer*, struct ByteArray*); // Decrypt the data with the key of this Signer object.
 };
 
 struct CryptoCreator{
+  
   char id[2];
   char* (*getId)(struct CryptoCreator*); // Fetch the above id object and return it.
   Signer* (*newInstance)(struct ByteArray* user_PrivateKey); // Generate a new Signer object which includes generating a new keypair.
 };
+
+/*
+ * FUNCTION DECLARATIONS
+ ****************************************************************************************
+ */
+
+char* cryptoGetId(CryptoCreator* object);
+struct XYResult* getPublicKey(Signer* signer);
+Signer* newInstance(ByteArray* user_PrivateKey);
+CryptoCreator* newCryptoCreator(void);
+
+#endif
+
+// end of file crypto.h
+
+
--- src/crypto.c
+++ src/crypto.c
@@ -1,42 +1,181 @@
+/**
+ ****************************************************************************************
+ *
+ * @file crypto.c
+ *
+ * @XY4 project source code.
+ *
+ * @brief primary crypto routines for the XY4 firmware.
+ *
+ * Copyright (C) 2017 XY - The Findables Company
+ *
+ * This computer program includes Confidential, Proprietary Information of XY. 
+ * All Rights Reserved.
+ *
+ ****************************************************************************************
+ */
+
+/*
+ * INCLUDES
+ ****************************************************************************************
+ */
+
 #include "crypto.h"
-#include <stdlib.h>
-#include <stddef.h>
-#include <string.h>
 
+/*
+ * FUNCTIONS & METHODS
+ ****************************************************************************************
+ */
 
+/**
+ ****************************************************************************************
+ *  NAME
+ *      cryptoGetId
+ *
+ *  DESCRIPTION
+ *      this routine returns the id of the object supplied
+ *
+ *  PARAMETERS
+ *      object      [in]      CryptoCreator*
+ *
+ *  RETURNS
+ *      id          [out]     char*
+ *
+ *  NOTES
+ *       
+ ****************************************************************************************
+ */
 char* cryptoGetId(CryptoCreator* object){
+  
   return object->id;
 }
 
+/**
+ ****************************************************************************************
+ *  NAME
+ *      getPublicKey
+ *
+ *  DESCRIPTION
+ *      this routine returns the public key for the supplied signer
+ *
+ *  PARAMETERS
+ *      signer            [in]      Signer*
+ *
+ *  RETURNS
+ *      return_result     [out]     XYResult*
+ *
+ *  NOTES
+ *      will return NULL if there is insufficient memory to create an XYResult
+ ****************************************************************************************
+ */
 struct XYResult* getPublicKey(Signer* signer){
+  
   XYResult* return_result = malloc(sizeof(XYResult));
+  
   if(return_result->error != OK) return return_result;
+  
   return_result->error = OK;
   return_result->result = signer->publicKey;
+  
   return return_result;
 }
 
+/**
+ ****************************************************************************************
+ *  NAME
+ *      generateKeypair
+ *
+ *  DESCRIPTION
+ *      this routine returns a NEW pair of public and private keys 
+ *
+ *  PARAMETERS
+ *      none
+ *
+ *  RETURNS
+ *      keypair     [out]     keyPairStruct*
+ *
+ *  NOTES
+ *      will return NULL if there is insufficient memory to create a keypair
+ ****************************************************************************************
+ */
 keyPairStruct* generateKeypair(){
+  
     keyPairStruct* keypair = malloc(sizeof(keyPairStruct));
+  
+  if (keypair) {
+    
     keypair->publicKey = NULL;
     keypair->privateKey = NULL;
-    return keypair;
+  }
+  
+  return keypair;
 }
 
+/**
+ ****************************************************************************************
+ *  NAME
+ *      newInstance
+ *
+ *  DESCRIPTION
+ *      this routine returns a NEW Signer 
+ *
+ *  PARAMETERS
+ *      user_PrivateKey     [in]      ByteArray*
+ *
+ *  RETURNS
+ *      signer              [out]     Signer*
+ *
+ *  NOTES
+ *      will return NULL if there is insufficient memory to create a signer
+ ****************************************************************************************
+ */
 Signer* newInstance(ByteArray* user_PrivateKey){
-  Signer* sig = malloc(sizeof(Signer));
-  sig->publicKey = NULL;
-  sig->privateKey = NULL; // Generate keypair and set these.
-  sig->getPublicKey = &getPublicKey;
-  sig->sign = NULL;
-  sig->verify = NULL;
-  return sig;
+  
+  Signer* signer = malloc(sizeof(Signer));
+  
+  if (signer) {
+    
+    signer->publicKey = NULL;
+    signer->privateKey = NULL;            // Generate keypair and set these.
+    signer->getPublicKey = &getPublicKey;
+    signer->sign = NULL;
+    signer->verify = NULL;
+  }
+  
+  return signer;
 }
 
+/**
+ ****************************************************************************************
+ *  NAME
+ *      newCryptoCreator
+ *
+ *  DESCRIPTION
+ *      this routine returns a NEW CryptoCreator 
+ *
+ *  PARAMETERS
+ *      none
+ *
+ *  RETURNS
+ *      creator     [out]     CryptoCreator*
+ *
+ *  NOTES
+ *      will return NULL if there is insufficient memory to create a creator
+ ****************************************************************************************
+ */
 CryptoCreator* newCryptoCreator(){
+  
   CryptoCreator* creator = malloc(sizeof(CryptoCreator));
-  memset(creator->id, 0x00, 2);
-  creator->newInstance = &newInstance;
-  creator->getId = &cryptoGetId;
+  
+  if (creator) {
+    
+    memset(creator->id, 0x00, 2);
+    creator->newInstance = &newInstance;
+    creator->getId = &cryptoGetId;
+  }
+  
   return creator;
 }
+
+// end of file crypto.c
+
